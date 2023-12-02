eea946142d29e1dd4c53968bfa65a26af37df679
Maria <abmcarolina256@gmail.com>
2023-12-01 21:33:49 -0300

    Subindo arquivos (crud idioma - com erro ao executar, mas to subindo pra não perder)

---
+++ CinemaMvc/Controllers/IdiomaController.cs
@@ -0,0 +1,82 @@
+﻿using CinemaMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CinemaMvc.Controllers
+{
+    public class IdiomaController : Controller
+    {
+        private static IList<Idioma> idiomas = new List<Idioma>()
+        {
+            new Idioma(){
+                Id =1,
+                Descricao="chuchuuuu"
+            },
+            new Idioma(){
+                Id = 2,
+                Descricao="chucafk,lkgwshuuuu"
+            },
+        };
+
+
+        // GET: Idioma
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Idioma idioma)
+        {
+            idiomas.Add(idioma);
+            idioma.Id = idiomas.Select(m => m.Id).Max() + 1;
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(long id)
+        {
+            return View(idiomas.Where(m => m.Id == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Idioma idioma)
+        {
+            idiomas.Remove(idiomas.Where(m => m.Id == idioma.Id).First());
+            idiomas.Add(idioma);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(long id)
+        {
+            return View(idiomas.Where(m => m.Id == id).First());
+        }
+
+        public ActionResult Delete(long id)
+        {
+            return View(idiomas.Where(m => m.Id == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Idioma idioma)
+        {
+            idiomas.Remove(idiomas.Where(m => m.Id == idioma.Id).First());
+            return RedirectToAction("Index");
+        }
+
+
+
+
+    }
+}
---
+++ CinemaMvc/Models/Idioma.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CinemaMvc.Models
+{
+    public class Idioma
+    {
+        public long Id { get; set; }
+        public string Descricao { get; set; }
+    }
+}
