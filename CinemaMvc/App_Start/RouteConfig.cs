ccc6bc1e2e950cf686da985abfcc91594cdadb30
Maria <abmcarolina256@gmail.com>
2023-12-06 20:35:27 -0300

    Adicionando rota padrão e alterando h2 das views

--- CinemaMvc/App_Start/RouteConfig.cs
+++ CinemaMvc/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CinemaMvc
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Atuacao", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
